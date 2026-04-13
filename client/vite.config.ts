93d5a0355252a301de8279e5a3264474d3b5d7d4
phlox1000 <mrmahoganyman@gmail.com>
2026-04-13 07:17:21 +0000

    Apply Vite chunk size fix directly to main

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,6 +10,12 @@ export default defineConfig({
     rollupOptions: {
       output: {
         format: 'esm',
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) return 'vendor';
+          if (id.includes('client/src/pages')) return 'pages';
+          if (id.includes('client/src/components')) return 'components';
+          return 'index';
+        },
         entryFileNames: '[name].js',
       },
     },
